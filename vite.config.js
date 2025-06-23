ca025ef4aead835e4e9f26481ed3637c5de7c5fb
Micho-Tade <tadessetabuse@gmail.com>
2025-06-24 00:42:52 +0300

    Edited it t oamke it work after 1 mon

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// 👇 Replace with your repository name
-const repo = 'Netflix-Clone-2025';
-
 export default defineConfig({
   plugins: [react()],
-  base: `/${repo}/`,   // <‑‑ crucial for GH Pages paths
+  base: '/' // ✅ for Netlify
 });
